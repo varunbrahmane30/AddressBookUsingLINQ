0901a5e01fc8e424493d2fdf292d4294376b828a
name <varunbrahmane30@gmail.com>
2021-08-19 13:22:41 +0530

    [varun] added: Address book contacts

---
+++ AddressBookUsingLINQ/AddressBookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsingLINQ
+{
+    class AddressBookModel
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone_Number { get; set; }
+        public string Email { get; set; }
+        public string BookName { get; set; }
+        public string BookType { get; set; }
+    }
+}
